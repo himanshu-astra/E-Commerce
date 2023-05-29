e3897a0a903a7cd4c919e2f474a0699b9a322f76
himanshu-astra <107385151+himanshu-astra@users.noreply.github.com>
2023-05-29 22:00:07 +0530

    Add to cart functionlity added

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -10,6 +10,15 @@ const Product = (props) => {
       <h4>{props.title}</h4>
       <p>${props.price}</p>
       <p>{props.category.name}</p>
+      {props.isInCart ? (
+        <button onClick={() => props.handleRemoveFromCart(props.id)}>
+          Remove from cart
+        </button>
+      ) : (
+        <button onClick={() => props.handleAddToCart(props.id)}>
+          Add to cart
+        </button>
+      )}
     </div>
   );
 };
@@ -41,6 +50,14 @@ const ProductList = () => {
     price_max: 0,
     price_min: 0,
   });
+  const [cart, setCart] = useState([]);
+  const [isLocalCartInitiaized, setIsLocalCArtInitialized] = useState(false);
+
+  const cartSet = new Set(cart);
+
+  const checkIfProductInCart = (id) => {
+    return cartSet.has(id);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -57,6 +74,22 @@ const ProductList = () => {
     fetchData();
   }, [activeFilters.title, activeFilters.price_max, activeFilters.price_min]);
 
+  useEffect(() => {
+    const localCart = JSON.parse(localStorage.getItem("cart"));
+    if (localCart === null || localCart === undefined) {
+      setCart([]);
+    } else {
+      setCart(localCart);
+    }
+    setIsLocalCArtInitialized(true);
+  }, []);
+
+  useEffect(() => {
+    if (isLocalCartInitiaized) {
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  }, [cart]);
+
   const handleTitleChange = (e) =>
     setActiveFilters({
       ...activeFilters,
@@ -68,6 +101,29 @@ const ProductList = () => {
       ...activeFilters,
       [e.target.name]: e.target.value,
     });
+
+  const handleAddToCart = (id) => {
+    const newCart = [...cart];
+    newCart.push(id);
+    setCart(newCart);
+  };
+
+  const handleRemoveFromCart = (id) => {
+    // [1, 2, 3]
+    // 2
+
+    // [1]
+    // [1]
+    // [1, 3]
+    // const newCart = cart.filter((cartItemId) => cartItemId !== id);
+    const newCart = [];
+    for (let i = 0; i < cart.length; i++) {
+      if (cart[i] !== id) {
+        newCart.push(cart[i]);
+      }
+    }
+    setCart(newCart);
+  };
 
   return (
     <div>
@@ -86,7 +142,13 @@ const ProductList = () => {
       />
       <div>
         {productData?.map((product, id) => (
-          <Product {...product} key={id} />
+          <Product
+            {...product}
+            key={id}
+            handleAddToCart={handleAddToCart}
+            handleRemoveFromCart={handleRemoveFromCart}
+            isInCart={checkIfProductInCart(product.id)}
+          />
         ))}
       </div>
     </div>
@@ -94,3 +156,10 @@ const ProductList = () => {
 };
 
 export default ProductList;
+
+// const cart = [1,2,3]
+// const newCart = cart
+// newCart.push(4);
+
+// console.log(oldCart) [1, 2, 3]
+// console.log(newCart) [1, 2, 3, 4]
